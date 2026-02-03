8ccbe8a0ca705e1e3139908e95191034efce471f
rezaaplvv <rezapahlepi77654@gmail.com>
2026-02-03 20:00:00 +0700

    Change code

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -134,7 +134,6 @@ export default function ProjectsPage() {
             </p>
         </div>
 
-        {/* GRID PROJECTS */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 auto-rows-[450px]">
             {ALL_PROJECTS.map((project, index) => (
               <motion.div
